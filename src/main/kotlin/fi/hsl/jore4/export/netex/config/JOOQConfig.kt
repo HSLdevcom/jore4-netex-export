73fa78c72ae495d2d80055d8b38ebe8fe84d0d0a
Teemu Mäkinen <teemu.makinen@vincit.fi>
2025-10-21 08:55:43 +0300

    Add Project Spring configuration

---
+++ src/main/kotlin/fi/hsl/jore4/export/netex/config/JOOQConfig.kt
@@ -0,0 +1,33 @@
+package fi.hsl.jore4.export.netex.config
+
+import org.jooq.ConnectionProvider
+import org.jooq.DSLContext
+import org.jooq.SQLDialect
+import org.jooq.impl.DataSourceConnectionProvider
+import org.jooq.impl.DefaultConfiguration
+import org.jooq.impl.DefaultDSLContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
+
+@Configuration
+class JOOQConfig(
+    val jooqProperties: JOOQProperties,
+) {
+    @Bean
+    fun connectionProvider(transactionAwareDataSourceProxy: TransactionAwareDataSourceProxy): ConnectionProvider =
+        DataSourceConnectionProvider(transactionAwareDataSourceProxy)
+
+    @Bean
+    fun configuration(connectionProvider: ConnectionProvider): org.jooq.Configuration {
+        val dialect = SQLDialect.valueOf(jooqProperties.dialect)
+
+        val config = DefaultConfiguration()
+        config.set(connectionProvider)
+        config.set(dialect)
+        return config
+    }
+
+    @Bean
+    fun dslContext(configuration: org.jooq.Configuration): DSLContext = DefaultDSLContext(configuration)
+}
